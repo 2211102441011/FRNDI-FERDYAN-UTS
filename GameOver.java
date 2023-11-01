679bcc9b6228b07ac145ca1de214e091bfe12a4d
frandi ferdyan <145576122+2211102441011@users.noreply.github.com>
2023-11-01 15:52:44 +0700

    Add files via upload

---
+++ GameOver.java
@@ -0,0 +1,8 @@
+import greenfoot.*;
+
+public class GameOver extends Actor {
+    public GameOver() {
+        GreenfootImage image = new GreenfootImage("Game Over.png"); // Ganti "gameover.png" dengan nama berkas gambar yang ingin Anda gunakan
+        setImage(image);
+    }
+}
